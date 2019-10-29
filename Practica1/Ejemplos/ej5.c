7ac5c3b245cf3da41c77d9fef478963a70b5248f
Juan Velasco Izquierdo <juan.velascoi@estudiante.uam.es>
2019-10-29 01:17:05 +0100

    Ahora si

--- Practica1/Ejemplos/ej5.c
+++ Practica1/Ejemplos/ej5.c
@@ -46,7 +46,7 @@ int main (int argc, char ** argv)
         multiplicar(fd_asm,1,0);
         escribir_operando(fd_asm,"m",1);
         escribir_elemento_vector(fd_asm,"v", 4, 1);
-        //asignarDestinoEnPila(fd_asm,0);
+        asignarDestinoEnPila(fd_asm,0);
 //m = m + 1
         escribir_operando(fd_asm,"m",1);
         escribir_operando(fd_asm,"1",0);
