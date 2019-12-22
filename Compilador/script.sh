949d6a34b1e36efdd2748fe82d17164b4b43c0ff
Juan Velasco Izquierdo <juan.velascoi@estudiante.uam.es>
2019-12-22 17:12:28 +0100

    Ejemplos pablo añadidos

--- Compilador/script.sh
+++ Compilador/script.sh
@@ -14,6 +14,26 @@ gcc -m32 -o ./ejemplos/boolean ./ejemplos/boolean.o alfalib.o
 nasm -felf32 ./ejemplos/ejemplo2.asm
 gcc -m32 -o ./ejemplos/ejemplo2 ./ejemplos/ejemplo2.o alfalib.o
 
-./alfa ./ejemplos/anidado.txt ./ejemplos/anidado.asm
-nasm -felf32 ./ejemplos/anidado.asm
-gcc -m32 -o ./ejemplos/anidado ./ejemplos/anidado.o alfalib.o
+./alfa ./ejemplos/condicionales.alfa ./ejemplos/condicionales.asm
+nasm -felf32 ./ejemplos/condicionales.asm
+gcc -m32 -o ./ejemplos/condicionales ./ejemplos/condicionales.o alfalib.o
+
+./alfa ./ejemplos/factorial.alfa ./ejemplos/factorial.asm
+nasm -felf32 ./ejemplos/factorial.asm
+gcc -m32 -o ./ejemplos/factorial ./ejemplos/factorial.o alfalib.o
+
+./alfa ./ejemplos/vectores.alfa ./ejemplos/vectores.asm
+nasm -felf32 ./ejemplos/vectores.asm
+gcc -m32 -o ./ejemplos/vectores ./ejemplos/vectores.o alfalib.o
+
+./alfa ./ejemplos/funciones.alfa ./ejemplos/funciones.asm
+nasm -felf32 ./ejemplos/funciones.asm
+gcc -m32 -o ./ejemplos/funciones ./ejemplos/funciones.o alfalib.o
+
+./alfa ./ejemplos/funciones_vectores.alfa ./ejemplos/funciones_vectores.asm
+nasm -felf32 ./ejemplos/funciones_vectores.asm
+gcc -m32 -o ./ejemplos/funciones_vectores ./ejemplos/funciones_vectores.o alfalib.o
+
+./alfa ./ejemplos/fibonacci.alfa ./ejemplos/fibonacci.asm
+nasm -felf32 ./ejemplos/fibonacci.asm
+gcc -m32 -o ./ejemplos/fibonacci ./ejemplos/fibonacci.o alfalib.o
